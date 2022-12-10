2d5e09b07c9273917113971666be2fdde8fb8d9f
mukit24 <mukithasanpranto4@gmail.com>
2022-12-10 17:06:25 +0600

    backend_auth

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import getProducts, getProduct
+from .views import getProducts, getProduct, getUser, getUsers, register_user
+from .views import MyTokenObtainPairView
+
 app_name = 'base'
 
 urlpatterns = [
+    path('user/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/register/', register_user, name='register-user'),
+    path('user/profile/',getUser,name='user-profile'),
+    path('users/',getUsers,name='user-list'),
     path('products/',getProducts,name='products'),
     path('products/<int:id>/',getProduct,name='product'),
 ]
